5430d15da0d9ffe112044f5095053fe05087b468
omg12347 <omghumre74@gmail.com>
2024-07-03 02:15:44 +0530

    updated prompt

--- app.py
+++ app.py
@@ -58,14 +58,14 @@ submit2 = st.button("Percentage match")
 
 input_prompt1 = """ 
 
-You are an experienced Technical Human Resource Manager,your task is to review the provided resume against the job description. 
+You are an highly experienced Technical Human Resource Manager,your task is to review the provided resume against the job description. 
 Please share your professional evaluation on whether the candidate's profile aligns with the role. 
 Highlight the strengths and weaknesses of the applicant in relation to the specified job requirements.
 
 """
 
 input_prompt2 = """
-You are an skilled ATS (Applicant Tracking System) scanner with a deep understanding of data science and ATS functionality, 
+You are an highly skilled ATS (Applicant Tracking System) scanner with a deep understanding of data science and ATS functionality, 
 your task is to evaluate the resume against the provided job description. give me the percentage of match if the resume matches
 the job description. 
 First the output should come as percentage and then keywords missing and last final thoughts.
