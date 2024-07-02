625b42b6bbd6f31ee4995a4b54d031dff179e7a3
omg12347 <omghumre74@gmail.com>
2024-07-03 00:46:52 +0530

    converted uploaded file to image

--- app.py
+++ app.py
@@ -16,4 +16,27 @@ def get_gemini_response(input, pdf_content, propmt):
     return response.text
 
 def input_pdf_setup(uploaded_file):
-    images = pdf2image.convert_from_bytes(uploaded_file.read())
+    if uploaded_file is not None:
+    
+        images = pdf2image.convert_from_bytes(uploaded_file.read())
+
+        first_page = images[0]
+
+        img_byte_arr = io.BytesIO()
+        first_page.save(img_byte_arr,format='JPEG')
+        img_byte_arr = img_byte_arr.getvalue()
+
+        pdf_parts = [
+
+            {
+                "mime_type" : "image/jpeg",
+                "data" : base64.b64encode(img_byte_arr).decode()
+            }
+
+        ]
+
+        return pdf_parts
+    else:
+        raise FileNotFoundError("No file uploaded")
+
+
