573b7f6af8314278c23e21c728352b535be1771a
omg12347 <omghumre74@gmail.com>
2024-07-03 00:39:17 +0530

    created imput function

--- app.py
+++ app.py
@@ -8,3 +8,12 @@ from PIL import Image
 import pdf2image
 import google.generativeai as genai
 
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+def get_gemini_response(input, pdf_content, propmt):
+    model=genai.GenerativeModel('gemini-pro-vision')
+    response = model.generate_content([input, pdf_content[0], propmt])
+    return response.text
+
+def input_pdf_setup(uploaded_file):
+    images = pdf2image.convert_from_bytes(uploaded_file.read())
